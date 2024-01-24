b5ae693f55f8277978bf1bd6fd72a873a6e69946
ms608122 <ms608122@ohio.edu>
2024-01-23 19:19:12 -0500

    final one

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: McKenzie Searcy
+ *        Date: 01-23-24
+ * Description: GITHUB Introduction Lab 
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
